b004f49d5e6998308856afd8bec14d4e34dfae29
Aashishsharma <ashishrsharma2@gmail.com>
2022-03-25 20:57:54 +0530

    a

--- React/react-app/src/components/Home.jsx
+++ React/react-app/src/components/Home.jsx
@@ -8,14 +8,26 @@ let formElemsValues = {
 export const Home = () => {
 	const [values, handleChange, handleSubmit] = useForm(formElemsValues, () => {
 	  console.log({values})
+	})
+
+	const [values2, handleChange2, handleSubmit2] = useForm(formElemsValues, () => {
+	  console.log({values2})
 	})	
 	
-	return (
+	return (<>
 		<form onSubmit={handleSubmit} >
 			<label> Name: - </label>
 			<input name="name" type = "text" onChange={handleChange} value={values.name} />
 			<input name="email" type = "email" onChange={handleChange} value={values.email} />
 			<button type="submit">Submit</button>
 		</form>
+
+		<form onSubmit={handleSubmit2} >
+			<label> Name: - </label>
+			<input name="name" type = "text" onChange={handleChange2} value={values2.name} />
+			<input name="email" type = "email" onChange={handleChange2} value={values2.email} />
+			<button type="submit">Submit</button>
+		</form>
+		</>
 	)
 }
